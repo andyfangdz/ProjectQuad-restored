5599864a182ba20fdf930a2b5b01c4d5e0efbad9
Andy Fang <yzmtf2008@gmail.com>
2014-04-26 23:21:41 +0800

    Add Trajectory Drawing

--- src/bishift.py
+++ src/bishift.py
@@ -31,6 +31,59 @@ import cv2.cv as cv
 import video
 from utils import mark
 
+WHITE = (255, 255, 255)
+BLUE = (255, 0, 0)
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+
+def project(pt, imgsize):
+    center = imgsize / 2
+    (x, y, z) = pt
+    return (int(z * 1.414 / 2 + x + center), int (center -(z * 1.414 /2 +y)))
+
+def drawFancyAxis(img, imgsize, size):
+    '''
+    Draw a fancy axis with x - blue, y - red, z- green)
+    '''
+    k = 20
+
+    #x
+    cv2.line(img, project((-imgsize/2, 0, 0), imgsize), project((imgsize/2, 0, 0), imgsize), BLUE, size)
+    cv2.line(img, project((imgsize/2, 0, 0), imgsize), project((imgsize/2 - k, -k, 0), imgsize), BLUE, size)
+    cv2.line(img, project((imgsize/2, 0, 0), imgsize), project((imgsize/2 - k, k, 0), imgsize), BLUE, size)
+
+    #y
+    cv2.line(img, project((0, -imgsize/2, 0), imgsize), project((0, imgsize/2, 0), imgsize), GREEN, size)
+    cv2.line(img, project((0, imgsize/2, 0), imgsize), project((k, imgsize/2 - k, 0), imgsize), GREEN, size)
+    cv2.line(img, project((0, imgsize/2, 0), imgsize), project((-k, imgsize/2 - k, 0), imgsize), GREEN, size)
+
+    #z
+    cv2.line(img, project((0, 0, -imgsize/2), imgsize), project((0, 0, imgsize/2), imgsize), RED, size)
+    cv2.line(img, project((0, 0, imgsize/2), imgsize), project((k, 0, imgsize/2 - k), imgsize), RED, size)
+    cv2.line(img, project((0, 0, imgsize/2), imgsize), project((-k, 0, imgsize/2 - k), imgsize), RED, size)
+
+    #drawArrow(img, size, (imgsize / 2, imgsize / 2), imgsize / 2,  0, BLUE)
+    #drawArrow(img, size, (imgsize / 2, imgsize / 2), imgsize * 1.414 / 2, math.pi / 4, GREEN)
+    #drawArrow(img, size, (imgsize / 2, imgsize / 2), imgsize / 2,  math.pi / 2, RED)
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
+
+
+
+
+
+# Create a black image
+traj = np.zeros((512,512,3), np.uint8)
+drawFancyAxis(img, 512, 1)
+cv2.imshow("Image", traj)
+
+cv2.waitKey(0)
+
+
 size_treshold = 4
 side_inc = 2
 size_maxium = 256
